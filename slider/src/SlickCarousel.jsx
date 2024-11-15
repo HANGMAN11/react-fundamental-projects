e12b38b32c7759676f18c32d3f3d9fe48daeed6b
HANGMAN11 <oleja.229@gmail.com>
2024-11-16 00:39:18 +0200

    have finished on fining the React Slick library

--- slider/src/SlickCarousel.jsx
+++ slider/src/SlickCarousel.jsx
@@ -2,44 +2,42 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
-import {list} from './data'
-import { useEffect, useState } from 'react'
-import {FaQuoteRight} from 'react-icons/fa'
+import { list } from "./data";
+import { useEffect, useState } from "react";
+import { FaDeaf, FaQuoteRight } from "react-icons/fa";
 
-const SlickCarousel = () =>{
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      };
-      return (
-        <section className="slick-container">
+const SlickCarousel = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll:1,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,   
+    pauseOnHover: true 
+  };
+  return (
+    <section className="slick-container">
+      <Slider {...settings}>
+        {list.map((person) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article
+              key={id}
+            >
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+      </Slider>
+    </section>
+  );
+};
 
-        <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
-        </section>
-      )
-}
-
-
-export default SlickCarousel 
+export default SlickCarousel;
