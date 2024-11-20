af78703de9cf918835fef6701c522cf97159e176
HANGMAN11 <oleja.229@gmail.com>
2024-11-20 16:07:57 +0200

    worked on the list rendering

--- color generator/src/App.jsx
+++ color generator/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10))
   return <main>
     <Form/> 
-    <ColorList/>
+    <ColorList colors={colors}/>
   </main>;
 };
 export default App;
