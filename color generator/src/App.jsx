9db428c41e1ac80866b9b6ea06eb65cba2a2aad2
HANGMAN11 <oleja.229@gmail.com>
2024-11-20 15:44:57 +0200

    found a value.js library

--- color generator/src/App.jsx
+++ color generator/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import Form from './Form';
 import ColorList from './ColorList';
+import Values from 'values.js';
 
 const App = () => {
+  const [colors, setColors] = useState(new Values('#f15025').all(10))
   return <main>
     <Form/> 
     <ColorList/>
