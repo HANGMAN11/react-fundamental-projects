6175e42a423e518634836e75db0d8446c1d581b9
HANGMAN11 <oleja.229@gmail.com>
2024-11-20 16:41:13 +0200

    used a clipboard API

--- color generator/src/SingleColor.jsx
+++ color generator/src/SingleColor.jsx
@@ -1,6 +1,23 @@
+import { ToastContainer, toast } from 'react-toastify';
+
+
 const SingleColor = ({index, color}) =>{
     const {hex, weight} = color;
-    return <article className={index > 10 ? 'color color-light':'color'} style={{background:`#${hex}`}}>
+    const saveToClipboard = async() =>{
+        if(navigator.clipboard){
+            try{
+                await navigator.clipboard.writeText(`#${hex}`);
+                toast.success('Color is copied to clipboard successfully')
+            }catch{
+                toast.error('Failed to copy color to clipboard')
+            }
+        }
+        else{
+            toast.error('Clipboard access is not available')
+        }
+    }
+    return <article className={index > 10 ? 'color color-light':'color'} style={{background:`#${hex}`}}
+    onClick={saveToClipboard}>
 
         <p className="percent-value">{weight}%</p>
         <p className="color-value">#{hex}</p>
